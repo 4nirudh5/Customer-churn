450f43b482a3ef2af89fd3c14bd253cd5feadcbc
4nirudh5 <92074942+4nirudh5@users.noreply.github.com>
2022-06-10 15:14:39 +0530

    Update main.py

--- cc/main.py
+++ cc/main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import pandas as pd
 
 header = st.container()
 dataset = st.container()
@@ -8,3 +8,22 @@ modelTraining = st.container()
 
 with header:
 	st.title('Welcome')
+
+
+with dataset:
+	st.header('Customer churn dataset')
+	st.text('data set from kaggle')
+
+    df = pd.read_csv('data/customer_churn.csv')
+    st.write(df.head())
+
+    total_purchase = pd.DataFrame(df['Total_Purchase'].value_counts()).head()
+    st.bar_chart(total_purchase)
+
+with features:
+    st.header('The features for the model')
+ 
+
+with model_training:
+    st.header('Model training') 
+    st.text('choose hyper parameters to see how they perform')    	
