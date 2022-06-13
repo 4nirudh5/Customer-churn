09c500fd276cced4d2d7047d49c7515dee05f184
4nirudh5 <92074942+4nirudh5@users.noreply.github.com>
2022-06-13 22:54:04 +0530

    Update main.py

--- cc/main.py
+++ cc/main.py
@@ -1,29 +1,26 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
+import pickle
+import base64 
 
-header = st.container()
-dataset = st.container()
-features = st.container()
-modelTraining = st.container()
+@st.cache(suppress_st_warning=True)
+def get_fvalue(val):
+    feature_dict = {"No":1,"Yes":2}
+    for key,value in feature_dict.items():
+        if val == key:
+            return value
 
-with header:
-	st.title('Welcome')
+def get_value(val,my_dict):
+    for key,value in my_dict.items():
+        if val == key:
+            return value
 
+app_mode = st.sidebar.selectbox('Select Page',['Home','Prediction']) #two pages
 
-with dataset:
-	st.header('Customer churn dataset')
-	st.text('data set from kaggle')
-
-    df = pd.read_csv('data/customer_churn.csv')
-    st.write(df.head())
-
-    total_purchase = pd.DataFrame(df['Total_Purchase'].value_counts()).head()
-    st.bar_chart(total_purchase)
-
-with features:
-    st.header('The features for the model')
- 
-
-with model_training:
-    st.header('Model training') 
-    st.text('choose hyper parameters to see how they perform')    	
+if app_mode=='Home':
+    st.title('Customer churn prediction :')  
+    st.image('/Users/anirudh/Desktop/cc/Elements/cci.png')
+    st.
+    st.markdown('Applicant Income VS Loan Amount ')
+    st.bar_chart(data[['ApplicantIncome','LoanAmount']].head(20))
